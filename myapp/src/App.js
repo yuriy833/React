c2708725843301c6cd6f9282e52a690a62bdf3df
Yuriy833 <mr.prezident2018@mail.ru>
2022-10-20 22:44:59 +0300

    add answer lesson5

---
+++ myapp/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import * as React from 'react';
+import { Routes, Route } from "react-router-dom";
+import NavBar from './components/NavBar';
+import HomePage from './pages/Home';
+import ChatsPage from './pages/Chats';
+import ProfilePage from './pages/Profile';
+import NotFound from './pages/NotFound';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<HomePage userName={"User"} />} />
+          <Route path='chats' element={<ChatsPage />}>
+            <Route path=':chatId' element={<ChatsPage />} />
+          </Route>
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </>
+    </div>
+  );
+}
+
+export default App;
